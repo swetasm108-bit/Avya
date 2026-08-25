a613c2c27865b8e4985dbcc3844014248d718953
swetasm108-bit <swetasm108@gmail.com>
2026-08-26 00:36:17 +0530

    Add Signup, SignIn, Account components with password toggle and validation

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import "./App.css";
+import Account from "./components/Pages/Account";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<h2>Jai Shree Ganesha</h2>} />
+          <Route path="/shop" element={<h2>Shop page — coming soon</h2>} />
+          <Route path="/wishlist" element={<h2>Wishlist page — coming soon</h2>} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
